b5664647feaa0be48b69debd83064cb4bee9552e
dayathapa1234 <dayathapa1234@gmail.com>
2022-11-28 19:30:13 +0000

    Added Role entity

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.shopme.admin.user;
+
+import com.shopme.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role,Integer> {
+}
