f267a248d8f75a8820dea98e692ae1121acd21f2
dayathapa1234 <dayathapa1234@gmail.com>
2022-12-02 00:58:07 +0000

    Added New User Form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,10 +1,12 @@
 package com.shopme.admin.user;
 
+import com.shopme.Role;
 import com.shopme.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,5 +21,21 @@ public class UserController {
         List<User> userList = userService.listAll();
         model.addAttribute("userList",userList);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model){
+        List<Role> listRoles = userService.listRoles();
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user",user);
+        model.addAttribute("listRoles",listRoles);
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        System.out.println(user);
+        return "redirect:/users";
     }
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import com.shopme.Role;
 import com.shopme.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,14 @@ public class UserService {
     @Autowired
     private  UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public List<User> listAll(){
         return (List<User>) userRepository.findAll();
     }
+
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepository.findAll();
+    };
 }
